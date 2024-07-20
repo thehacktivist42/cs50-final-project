1978f272a7a3752c7f963ef595b4fff3b362adda
thehacktivist42 <harivanshmehta4@gmail.com>
2024-07-20 23:51:32 +0530

    changeth

--- app.py
+++ app.py
@@ -12,16 +12,42 @@ def index():
         elem = int(request.form.get("element"))
         if elem >= 119:
             element = iupacName(elem)
-            m = d = mp = bp = name_origin = False
+            m = d = mp = bp = discovery_year = discoverers = False
+            elemName = element
+            name_origin = "Named using the conventions laid out by IUPAC for the nomenclature of undiscovered elements"
+            description = f"The properties of {elemName} are not known."
+            color = 'light'
+            block = ''
         else:
             element = elemSearch(elem)
             elemSym = elemSymbol(elem)
-            m = getattr(mendeleev, elemSym).mass
-            d = round(float(getattr(mendeleev, elemSym).density)*1000, 3)
-            mp = round(getattr(mendeleev, elemSym).melting_point, 2)
-            bp = round(getattr(mendeleev, elemSym).boiling_point, 2)
-            name_origin = getattr(mendeleev, elemSym).name_origin
-
+            elemName = getattr(mendeleev, elemSym).name
+            m = (getattr(mendeleev, elemSym).mass if getattr(mendeleev, elemSym).mass else "Not known")
+            d = (round(float(getattr(mendeleev, elemSym).density)*1000, 3) if getattr(mendeleev, elemSym).density else "Not known")
+            mp = (round(getattr(mendeleev, elemSym).melting_point, 2) if getattr(mendeleev, elemSym).melting_point else "Not known")
+            bp = (round(getattr(mendeleev, elemSym).boiling_point, 2) if getattr(mendeleev, elemSym).boiling_point else "Not known")
+            block = getattr(mendeleev, elemSym).block
+            name_origin = (getattr(mendeleev, elemSym).name_origin if getattr(mendeleev, elemSym).name_origin else "Not known")
+            description = (getattr(mendeleev, elemSym).description if getattr(mendeleev, elemSym).description else f"Not much is known about {elemName} due to its recent discovery and short half-life.")
+            discovery_year = (getattr(mendeleev, elemSym).discovery_year if getattr(mendeleev, elemSym).discovery_year else f"Not known")
+            discoverer = (getattr(mendeleev, elemSym).discoverers if getattr(mendeleev, elemSym).discoverers else "Not known")
+            discoverers = ''
+            if type(discoverer) is list:
+                discoverers = ', '.join(discoverer)
+            else:
+                discoverers = discoverer
+            color = ''
+            match block:
+                case 's':
+                    color = 'danger'
+                case 'p':
+                    color = 'warning'
+                case 'd':
+                    color = 'primary'
+                case 'f':
+                    color = 'success'
+                case _:
+                    color = 'light'
         return render_template("indexed.html", 
                                element = element, 
                                elecConfig = electronicConfiguration(elem), 
@@ -30,6 +56,12 @@ def index():
                                density = d,
                                meltingPoint = mp,
                                boilingPoint = bp,
-                               nameOrigin = name_origin
+                               nameOrigin = name_origin,
+                               elementName = elemName,
+                               description = description,
+                               discoverers = discoverers,
+                               discoveryYear = discovery_year,
+                               colour = color,
+                               block = block
                                )
         
